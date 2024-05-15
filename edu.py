3b297a17d75b734702b034753aa2ec7388f6aa47
vizz-bob <vizz.bob@gmail.com>
2024-05-15 17:48:25 +0000

    3rd commit

--- edu.py
+++ edu.py
@@ -1,4 +1,5 @@
 print("Welcome to KL")
 This is 2nd line in git hub
 This is 3rd line from terminal ubuntu
-This is 4th line from git hub. 
+This is 4th line from git hub.
+This is 5th link from terminal
