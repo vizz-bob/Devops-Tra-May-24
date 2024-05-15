f9df025b77fde512b9a41a00fcc9b60d76452a2f
Vijayindraa Siingh <39841675+vizz-bob@users.noreply.github.com>
2024-05-16 01:49:23 +0800

    Update edu.py

--- edu.py
+++ edu.py
@@ -2,4 +2,5 @@ print("Welcome to KL")
 This is 2nd line in git hub
 This is 3rd line from terminal ubuntu
 This is 4th line from git hub.
-This is 5th link from terminal
+This is 5th line from terminal
+Thi is 6th line from git hub
