d42f4fbafed4734fdb7b21fc855c5823d7f995e5
vizz-bob <vizz.bob@gmail.com>
2024-05-15 18:06:43 +0000

    4th commit

--- edu.py
+++ edu.py
@@ -3,4 +3,5 @@ This is 2nd line in git hub
 This is 3rd line from terminal ubuntu
 This is 4th line from git hub.
 This is 5th line from terminal
-Thi is 6th line from git hub
+This is 6th line from git hub
+This is 7th line from terminal.
